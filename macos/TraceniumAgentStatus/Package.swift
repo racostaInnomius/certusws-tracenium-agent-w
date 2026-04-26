9bd44e5cb37de58e857482eff4c9aa459a784728
Javier Pacheco Ramirez <javier.pacheco@certusitm.com>
2026-04-25 23:49:56 -0600

    macos tryicon - draft

---
+++ macos/TraceniumAgentStatus/Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "TraceniumAgentStatus",
+    platforms: [
+        .macOS(.v13)
+    ],
+    products: [
+        .executable(name: "TraceniumAgentStatus", targets: ["TraceniumAgentStatus"])
+    ],
+    targets: [
+        .executableTarget(
+            name: "TraceniumAgentStatus",
+            path: "Sources"
+        )
+    ]
+)
