00260b7689f9ec641370917baddf1279ce0a84f5
Rodrigo Acosta <rodrigo191@gmail.com>
2026-08-13 18:36:53 -0600

    chore: bump agent version to 1.1.34

--- privsvc/windows/Tracenium.PrivSvc.Windows/Ipc/Router.cs
+++ privsvc/windows/Tracenium.PrivSvc.Windows/Ipc/Router.cs
@@ -140,7 +140,7 @@ public sealed class Router
             "ping" => Task.FromResult(PrivSvcResponse.Success(req.Id, new
             {
                 service = "TraceniumPrivSvc",
-                version = "1.1.33",
+                version = "1.1.34",
                 utc = DateTime.UtcNow.ToString("O")
             })),
 
