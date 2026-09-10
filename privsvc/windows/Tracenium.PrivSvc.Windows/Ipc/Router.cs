403471a60c92ba1e3248c526a44f4944347f0319
Javier Pacheco Ramirez <javier.pacheco@certusitm.com>
2026-09-10 10:01:27 -0600

    Bump 1.1.68

--- privsvc/windows/Tracenium.PrivSvc.Windows/Ipc/Router.cs
+++ privsvc/windows/Tracenium.PrivSvc.Windows/Ipc/Router.cs
@@ -151,7 +151,7 @@ public sealed class Router
             "ping" => Task.FromResult(PrivSvcResponse.Success(req.Id, new
             {
                 service = "TraceniumPrivSvc",
-                version = "1.1.67",
+                version = "1.1.68",
                 utc = DateTime.UtcNow.ToString("O")
             })),
 
